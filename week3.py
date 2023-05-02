158eaf1645e8665d6348e2aa8e269ef720162ee7
SungSuSon <thstjdtn05@gmail.com>
2023-05-02 16:53:20 +0900

    update week3,pascal

--- week3.py
+++ week3.py
@@ -43,27 +43,75 @@ def solution(n):
         temp_arr = arr
     return arr
 
-x = 6
-print(solution(x))
 
 ## 코드 발전, 재귀 함수를 이용
 
-# def pascal(x):
-#     pass
-#
-#
-# def solution_2(n):
-#     if 3 > n :
-#         return [1] if n == 1 else [1,1]
-#     index = 2
-#     temp_arr = [1,1]
-#     arr = temp_arr
-#     while n > index:
-#         index += 1
-#         arr = [0] * index
-#         arr = pascal(index)
-#         temp_arr = arr
-#     return arr
-#
-# x = 6
-# print(solution(x))
+def pascal(n):
+    if n < 1:
+        return []
+    elif n == 1:
+        return [1]
+    else:
+        temp_arr = solution(n - 1)
+        # n = 4
+        # temp_arr = solution(3)
+
+        # n = 3
+        # temp_arr = solution(2)
+
+        # n = 2
+        # temp_arr = solution(1)
+
+        # n = 1
+        # return [1] >> n이 2일때로 재귀
+
+        # n = 2
+        # temp_arr = [1]
+        # result = [1] * n = [1,1]
+        # for i in range(0,n-1) : (0,1)
+        #   i = 0 > contionue
+        #   i = 1 > for문 종료
+        # return [1,1]  >> n이 3일때로 재귀
+
+        # n = 3
+        # temp_arr = [1,1]
+        # result = [1] * n = [1,1,1]
+        # for i in range(0,2)
+        # i = 0 > continue
+        # i = 1 > result[1] = temp_arr[0] + temp_arr[1]  >> result[1] = 1 + 1
+        # i = 2 > for문 종료 , 결과값.  result = [1,2,1]
+
+        # n = 4
+        # temp_arr = [1,2,1]
+        # result = [1] * n = [1,1,1,1]
+        # for i in range(0,3)
+        # i = 0 > continue
+        # i = 1 > result[1] = temp_arr[0] + temp_arr[1]  >> result[1] = 1 + 2
+        # i = 2 > result[2] = temp_arr[1] + temp_arr[2]  >> result[2] = 2 + 1
+        # i = 3 > for문 종료, 결과값. result = [1,3,3,1]
+
+        result = [1] * n
+        for i in range(1, n - 1):
+            """ 새로운 리스트 만들기 """
+            # if i == 0 or i == n-1 :
+            #     continue
+            result[i] = temp_arr[i-1] + temp_arr[i]
+        return result
+
+# 반복문
+x = 7
+print(solution(x))
+
+# 재귀 함수
+x = 7
+print(pascal(x))
+
+
+
+
+
+
+
+for i in range(0,1):
+    print(i)
+
